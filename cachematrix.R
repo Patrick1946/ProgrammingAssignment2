8d357b303efe8d1c0dc638c98f393e6c5efa1204
Patrick Pollazzon <p.pollazzon89@gmail.com>
2014-04-26 15:14:46 -0700

    message

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions are potentially usuful time-consuming computations. 
 
-## Write a short comment describing this function
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- mean
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
+
+## it is easy verify it with an example:
+## x<-matrix((sample((1:100),16,replace=TRUE)),4,4)
+## so you create a squared matrix
+## then you calculate the product between this matrix and its inverse, obtained as cacheSolve(makeCacheMatrix(x))
+## the result is the identity matrix (the product between the matrix is done with %*% symbol)
